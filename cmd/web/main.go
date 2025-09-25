3ce28a834141fd9638b313b0e810df333a04fc38
Aldo Camacho <me@aldocamacho.com>
2025-09-25 13:55:40 -0700

    serve static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,10 @@ func main() {
 	const PORT = ":4000"
 
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
