b105ccbf541aa5a1904766b410c5774b424494ae
Aldo Camacho <me@aldocamacho.com>
2025-09-29 15:06:53 -0700

    add form tests

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bytes"
+	"html"
 	"io"
 	"log/slog"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
+	"strings"
 	"testing"
 	"time"
 
@@ -35,6 +39,17 @@ func newTestApplication(t *testing.T) *application {
 		formDecoder:    formDecoder,
 		sessionManager: sessionManager,
 	}
+}
+
+var csrfTokenRX = regexp.MustCompile(`<input type="hidden" name="csrf_token" value="(.+)">`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+	matches := csrfTokenRX.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(matches[1])
 }
 
 type testServer struct {
@@ -72,4 +87,28 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 	body = bytes.TrimSpace(body)
 
 	return rs.StatusCode, rs.Header, string(body)
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	req, err := http.NewRequest(http.MethodPost, ts.URL+urlPath, strings.NewReader(form.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Add("Origin", ts.URL)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, err := ts.Client().Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	return res.StatusCode, res.Header, string(body)
 }
