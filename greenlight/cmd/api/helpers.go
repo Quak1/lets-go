2b8624ba35187e6da4d5769a6c7e001c1539cf96
Aldo Camacho <me@aldocamacho.com>
2025-09-30 13:18:14 -0700

    add custom JSON response

--- greenlight/cmd/api/helpers.go
+++ greenlight/cmd/api/helpers.go
@@ -10,8 +10,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) readIDParam(ps httprouter.Params) (int, error) {
-	id, err := strconv.Atoi(ps.ByName("id"))
+func (app *application) readIDParam(ps httprouter.Params) (int64, error) {
+	id, err := strconv.ParseInt(ps.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
@@ -19,8 +19,10 @@ func (app *application) readIDParam(ps httprouter.Params) (int, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
-	js, err := json.Marshal(data)
+type envelope map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/Quak1/lets-go/greenlight/internal/data"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -14,9 +16,21 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	id, err := app.readIDParam(ps)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "Movie details %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
