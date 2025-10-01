886f84cd5bc4c4b71a43470668f5c9e3cd062d10
Aldo Camacho <me@aldocamacho.com>
2025-10-01 11:35:37 -0700

    add update movie route

--- greenlight/cmd/api/routes.go
+++ greenlight/cmd/api/routes.go
@@ -15,6 +15,7 @@ func (app *application) routes() http.Handler {
 	router.GET("/v1/healthcheck", app.healthcheckHandler)
 	router.POST("/v1/movies", app.createMovieHandler)
 	router.GET("/v1/movies/:id", app.showMovieHandler)
+	router.PUT("/v1/movies/:id", app.updateMovieHandler)
 
 	return app.recoverPanic(router)
 }
