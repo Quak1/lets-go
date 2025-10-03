577b8e25753013054c729de99e2010e22c616215
Aldo Camacho <me@aldocamacho.com>
2025-10-02 17:26:02 -0700

    add register user handler

--- greenlight/internal/data/users.go
+++ greenlight/internal/data/users.go
@@ -29,14 +29,19 @@ type password struct {
 	hash      []byte
 }
 
-func (p *password) Set(plaintextPassword string) error {
-	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+func (p *password) SetHash() error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(*p.plaintext), 12)
 	if err != nil {
 		return err
 	}
-	p.plaintext = &plaintextPassword
+
 	p.hash = hash
+
 	return nil
+}
+
+func (p *password) SetPlain(plaintextPassword string) {
+	p.plaintext = &plaintextPassword
 }
 
 func (p *password) Matches(plaintextPassword string) (bool, error) {
@@ -49,6 +54,7 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 			return false, err
 		}
 	}
+
 	return true, nil
 }
 
@@ -56,11 +62,13 @@ func ValidateEmail(v *validator.Validator, email string) {
 	v.Check(email != "", "email", "must be provided")
 	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
 }
+
 func ValidatePasswordPlaintext(v *validator.Validator, password string) {
 	v.Check(password != "", "password", "must be provided")
 	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
 	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
 }
+
 func ValidateUser(v *validator.Validator, user *User) {
 	v.Check(user.Name != "", "name", "must be provided")
 	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
@@ -71,9 +79,9 @@ func ValidateUser(v *validator.Validator, user *User) {
 		ValidatePasswordPlaintext(v, *user.Password.plaintext)
 	}
 
-	if user.Password.hash == nil {
-		panic("missing password hash for user")
-	}
+	// if user.Password.hash == nil {
+	// 	panic("missing password hash for user")
+	// }
 }
 
 type UserModel struct {
