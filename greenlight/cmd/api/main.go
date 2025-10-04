72cdeb4ab3e015f213c113d88780e8c9b1479734
Aldo Camacho <me@aldocamacho.com>
2025-10-04 13:31:07 -0700

    add Makefile

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -60,7 +60,7 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
-	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "PostgreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection iddle time")
@@ -84,6 +84,7 @@ func main() {
 
 	requireFlag("smtp-username", cfg.smtp.username)
 	requireFlag("smtp-password", cfg.smtp.password)
+	requireFlag("db-dsn", cfg.smtp.password)
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
